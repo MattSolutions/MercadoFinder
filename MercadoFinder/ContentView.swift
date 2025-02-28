87cf176ba5d373a09044d0d5f598978e172fac3a
mbattiti <A05331@bh.com.ar>
2025-02-28 13:10:41 -0300

    Initial Commit

---
+++ MercadoFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MercadoFinder
+//
+//  Created by MATIAS BATTITI on 28/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MercadoFinder/MercadoFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoFinderApp.swift
+//  MercadoFinder
+//
+//  Created by MATIAS BATTITI on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MercadoFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
